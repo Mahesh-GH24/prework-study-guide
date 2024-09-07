dbb045e9c9cfddb75d0c9edd92fbb86825f39562
Mahesh Gunasekaran <maheshgunasekaran24@gmail.com>
2024-09-07 12:30:05 -0500

    Javascript functions

--- assets/script.js
+++ assets/script.js
@@ -25,6 +25,42 @@ for (let index = 0; index < shapes.length; index++) {
 
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for (let x = 0; x < topics.length; x++) {
- console.log(topics[x]);
+// for (let x = 0; x < topics.length; x++) {
+//  console.log(topics[x]);
+// }
+
+console.log("Math random value " + Math.random());
+console.log("Topics length value " + topics.length);
+
+const randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+console.log("randomtopic value " + randomTopic);
+
+function listTopics()
+{
+    for (let index = 0; index < topics.length; index++) {
+        console.log(topics[index]);
+    }
 }
+
+function selectTopic()
+{
+    if (randomTopic === 'HTML') {
+        console.log("Let's study HTML!");
+      } else if (randomTopic === 'CSS') {
+        console.log("Let's study CSS!");
+      } else if (randomTopic === 'Git') {
+        console.log("Let's study Git!");
+      } else if (randomTopic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+      } else {
+        console.log('Please try again!');
+      }
+}
+console.log('Here are the topics we learned through Prework:');
+
+listTopics();
+
+console.log('Which topic should we study first?');
+
+selectTopic();
